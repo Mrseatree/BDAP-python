4af6fe8a1e63928ba63c81d641682bc3d44239a3
Mrseatree <378139419@qq.com>
2025-07-18 10:53:17 +0800

    添加FAISS数据库创建及配置faiss文件，知识库文件，依赖说明

--- faiss_builder.py
+++ faiss_builder.py
@@ -2,12 +2,12 @@ import datetime
 import hashlib
 import os
 
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.document_loaders import (
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.docstore.document import Document
+from langchain_community.document_loaders import (
     PyPDFLoader,
     UnstructuredWordDocumentLoader,
     UnstructuredFileLoader
